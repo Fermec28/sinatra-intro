50eb53eb71bdcb1ac54f4fbe1c48df5cd2622663
Fermedina <fermed28@gmai.com>
2017-11-16 00:04:45 -0500

    Complete CRUD on entrys of blog challenge

--- challenges/2-blog/app/controllers/entries_controller.rb
+++ challenges/2-blog/app/controllers/entries_controller.rb
@@ -9,8 +9,6 @@ end
 
 get '/entries/:id' do 
 	@entry = Entry.find(params[:id])
-	
-
 	erb :"/entries/show"
 end
 
@@ -18,6 +16,13 @@ get '/tags/:id/entries' do
 	@entries= Tag.find(params[:id]).entries
 	erb :"/entries/index"
 end
+
+get '/entries/:id/edit' do
+	@entry=Entry.find(params[:id]) 
+	erb :"/entries/edit"
+end
+
+
 
 post '/entries' do 
  entry= Entry.new(params[:entry])
@@ -31,8 +36,52 @@ post '/entries' do
  		 	entry.tags << Tag.find_by(content: tag)
  		 end 
  	end
- end
- 
- redirect "/entries"
+ 	redirect "/entries"
+ else
+ 	@errors= entry.errors.full_messages
+ 	erb :"/index"
+ end 
 
 end
+
+
+patch '/entries/:id' do
+ entry= Entry.find(params[:id])
+ if entry.update(params[:entry])
+ 	#eliminar los que ya no esten
+ 	tags=entry.content.scan(/#\w+/)
+ 	entry.tags.each  do|tag_entry|
+ 		#recorro los tags viejos
+ 		unless tags.include? tag_entry.content  
+ 			#si los nuevos tags no incluyen el viejo elimine la relacion
+
+ 			entry.tags.delete(tag_entry)
+ 		end
+ 	end
+
+ 	tags.each do |tag|
+ 		unless Tag.find_by(content: tag) 			
+ 		 	entry.tags.create(content: tag)
+ 		 else 
+ 		 	if entry.tags.find_by(content: tag)  # Exiten Tags asociados a entry con el mismo TAG?
+ 		 		#si si :no haga nada porque ya estan asociados
+ 		 	else
+ 		 		#si no: asocielos
+ 		 	entry.tags << Tag.find_by(content: tag)
+ 		 	end
+ 		 end 
+ 	end
+ 	redirect "/entries/#{entry.id}"
+ else
+ 	@errors= entry.errors.full_messages
+ 	erb :"/entries/#{entry.id}/edit"
+ end
+	
+end
+
+delete '/entries/:id' do
+  Entry.delete(params[:id])
+  redirect "/"
+end
+
+
